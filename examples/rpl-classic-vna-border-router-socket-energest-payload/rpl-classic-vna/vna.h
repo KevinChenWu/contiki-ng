f6d6443feeec4f21d7704c26c35a755bd5603424
Kevin Chen Wu <hongwenchen.k.c.w.90@gmail.com>
2025-02-11 19:53:48 -0600

    Adjusted the value of UIP_CONF_BUFFER_SIZE from 140 to 512 (bytes). The payload can be calculated using UIP_CONF_BUFFER_SIZE - 56 = 456 bytes. For now, 512 bytes is a stable value for UIP_CONF_BUFFER_SIZE and it is 4 times the size of a packet in 6LoWPAN. The fragmentation is enabled by default in 6LoWPAN of Contiki NG. The RPL Border Router and nodes should use the same value for UIP_CONF_BUFFER_SIZE in their respective project-conf.h

---
+++ examples/rpl-classic-vna-border-router-socket-energest-payload/rpl-classic-vna/vna.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+#include <inttypes.h>
+extern uint8_t vna_mode;
---
+++ examples/rpl-classic-vna-border-router-socket-energest-payload/wsn_server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import socket
+
+server_ip = "fd00::1"
+server_port = 5678
+buffer_size = 1024
+
+udp_server = socket.socket(family=socket.AF_INET6, type=socket.SOCK_DGRAM)
+udp_server.bind((server_ip, server_port))
+
+print("UDP WSN server up and listening")
+
+while True:
+    data_bytes, address = udp_server.recvfrom(buffer_size)
+    node_id = address[0].split(":")[-1]
+    data = data_bytes.decode()
+    print(f"Node ID: {node_id}, Node IP: \"{address[0]}\", data: \"{data}\"")
